468e9e5b03e3906317ff3c544d66854d1f0644c9
Hossein Krouna <krouna.hossein@gmx.net>
2023-01-12 11:01:47 +0100

    add titlebar

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  compiler: {
+    styledComponents: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import TitleBar from "../components/TitleBar";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <TitleBar />
+      <Component {...pageProps} />
+    </>
+  );
 }
